ffbd97227cc37c4693aed00b994ce9a53f940d21
Snehal232 <48058436+Snehal232@users.noreply.github.com>
2019-06-05 15:57:20 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -89,7 +89,6 @@ if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
 define( 'MYSQL_SSL_CA', ABSPATH . 'ca.pem' );
-define( 'MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL );
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
