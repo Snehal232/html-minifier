178a212f5998555e531975b1b5f6c92d3062290f
Snehal232 <48058436+Snehal232@users.noreply.github.com>
2019-06-12 13:23:43 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'blog');
+define('DB_NAME', 'WordPress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'sgroot');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Technokeens@123');
+define('DB_PASSWORD', 'aHnUxwVlyReMC9-G');
 
 /** MySQL hostname */
 define('DB_HOST', 'SG-WordPress-741-master.servers.mongodirector.com');
